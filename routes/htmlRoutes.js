6946eb8a71a04878e216e0283e26a1564a1babe6
Said M Shah <saidmshah93@gmail.com>
2018-09-19 19:48:26 -0700

    Work

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,34 @@
-var db = require("../models");
+//var db = require("../models");
+var isAuthenticated = require('../config/middleware/isAuthenticated.js');
+
 
 module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
-  });
+    // Load index page
+    app.get("/", function(req, res) {
+        //db.Example.findAll({}).then(function(dbExamples) {
+        res.render("login");
+        //msg: "Welcome!",
+        //examples: dbExamples
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
     });
-  });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
+
+    // Load example page and pass in an example by id
+    //app.get("/example/:id", function(req, res) {
+    app.get("/signup", function(req, res) {
+        //db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+        res.render("sign-up", {
+            //example: dbExample
+        });
+    });
+
+
+    // Render 404 page for any unmatched routes
+    //app.get("*", function(req, res) {
+    //res.render("404");
+    //});
+
+    app.get("/home", isAuthenticated, function(req, res) {
+        res.render("index");
+    });
 };
--- server.js
+++ server.js
@@ -2,23 +2,31 @@ require("dotenv").config();
 var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
+var session = require("express-session");
+var passport = require("./config/passport.js");
 
 var db = require("./models");
-
+// Setting the Express app
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+//passport set-up
+app.use(session({ secret: 'mySecretKey', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 // Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
 // Handlebars
 app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
+    "handlebars",
+    exphbs({
+        defaultLayout: "main"
+    })
 );
 app.set("view engine", "handlebars");
 
@@ -31,18 +39,18 @@ var syncOptions = { force: false };
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
 if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
+    syncOptions.force = true;
 }
 
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+    app.listen(PORT, function() {
+        console.log(
+            "App listening on port " +
+            PORT,
+            PORT
+        );
+    });
 });
 
 module.exports = app;
