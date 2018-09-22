6928f6fbd5a93b2a061d2414d7cd5ab3b015970a
Alexandr Bakilin <37944811+dexxel@users.noreply.github.com>
2018-09-22 12:01:10 -0700

    Update appt.js

--- models/appt.js
+++ models/appt.js
@@ -1,7 +1,25 @@
-module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
-  });
-  return Example;
-};
+module.exports = function (sequelize, DataTypes) {
+  var Appt = sequelize.define("Appt", {
+    date: {
+      type: DataTypes.STRING,
+    },
+    time: {
+      type: DataTypes.STRING,
+    },
+    type: {
+      type: DataTypes.STRING,
+    },
+    reason: DataTypes.STRING
+  })
+
+
+  Appt.associate = function (models) {
+    Appt.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return Appt;
+};    
