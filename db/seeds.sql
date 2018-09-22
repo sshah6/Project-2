f156f6545203cdf3d49cc63bc919642d4319dea1
Said M Shah <saidmshah93@gmail.com>
2018-09-22 12:23:06 -0700

    changed db

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+Insert into Users (email, password) Values ('alex@healthbook.com', 'Alex');
+Insert into Users (email, password) Values ('hassan@healthbook.com', 'Hassan');
+Insert into Users (email, password) Values ('said@healthbook.com', 'Said');
+Insert into Users (email, password) Values ('ruby@healthbook.com', 'Ruby');
